d3b7d2fb93934d6392afb9ad089891e574a61ead
m.carlson <m.carlson@db2202da-8704-0410-b924-b125b3cd84ef>
2012-11-15 23:38:44 +0000

    Adds pRolocdata and RforProteomics.
    git-svn-id: https://hedgehog.fhcrc.org/bioc-data/trunk/experiment/pkgs/RforProteomics@2179 db2202da-8704-0410-b924-b125b3cd84ef

---
+++ R/PXD000001.R
@@ -0,0 +1,58 @@
+.getPXD000001 <- function(destdir, src, unpack = TRUE) {
+  dest <- basename(src)
+  dest <- file.path(destdir, dest)
+  dest2 <- gsub("[.]gz$", "", dest)
+  if (!file.exists(dest2)) {
+    if (!file.exists(dest))
+      download.file(src, destfile = dest)
+    if (unpack)
+      gunzip(dest)
+  }
+  invisible(dest2)
+}
+
+
+##' Unless already present, downloads the PXD000001 mzXML file
+##' in the \code{destdir} directory. The resulting file is named
+##' \code{TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01.mzXML}.
+##'
+##' @title Download the PXD000001 mzXML file
+##' @param destdir A \code{character} with the destination folder.
+##' @return Invisibly returns the name of the downloaded file.
+##' @author Laurent Gatto
+getPXD000001mzXML <- function(destdir = ".") {
+  ## src <- "http://proteome.sysbiol.cam.ac.uk/lgatto/RforProteomics/TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01.mzXML.gz"
+  src2 <- "ftp://ftp.pride.ebi.ac.uk/2012/03/PXD000001/TMT_Erwinia_1uLSike_Top10HCD_isol2_45stepped_60min_01.mzXML"
+  .getPXD000001(destdir, src2, FALSE)
+}
+
+
+##' Unless already present, downloads the PXD000001 mzTab file
+##' n the \code{destdir} directory. The resulting file is named
+##' \code{F063721.dat-mztab.txt}.
+##'
+##' @title Download the PXD000001 mzTab file
+##' @param destdir A \code{character} with the destination folder.
+##' @return Invisibly returns the name of the downloaded file.
+##' @author Laurent Gatto
+getPXD000001mzTab <- function(destdir = ".") {
+  src <- "ftp://ftp.pride.ebi.ac.uk/2012/03/PXD000001/F063721.dat-mztab.txt"
+  .getPXD000001(destdir, src, FALSE)
+}
+
+
+##' Unless already present, downloads the PXD000001 mzData file
+##' in the \code{destdir} directory. The resulting file is named
+##' \code{PRIDE_Exp_Complete_Ac_22134.xml}
+##'
+##' @title Download the PXD000001 mzTab file
+##' @param destdir A \code{character} with the destination folder.
+##' @return Invisibly returns the name of the downloaded file.
+##' @author Laurent Gatto
+getPXD000001mzData <- function(destdir = ".") {
+  src <- "ftp://ftp.pride.ebi.ac.uk/2012/03/PXD000001/PRIDE_Exp_Complete_Ac_22134.xml.gz"
+  .getPXD000001(destdir, src, TRUE)
+}
+
+
+
---
+++ R/r4prot.R
@@ -0,0 +1,8 @@
+##' Opens the package vignettes.
+##'
+##' @title Opens RforProteomics vignettes
+##' @return An instance of class \code{vignette},
+##' as returned by \code{\link{vignette}}
+##' @author Laurent Gatto
+RforProteomics <- function()
+  vignette("RforProteomics", package = "RforProteomics")
